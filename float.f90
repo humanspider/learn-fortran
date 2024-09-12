cfc075a928e50dae7ceb03f9142ea50c156d3175
toomanydads <mswolbaum@gmail.com>
2024-09-12 12:41:12 -0500

    Initial commit

---
+++ float.f90
@@ -0,0 +1,16 @@
+program float
+    ! The desired floating-point precision can be explicitly declared using a kind parameter. 
+    ! The iso_fortran_env intrinsic module provides kind parameters for the common 32-bit and 64-bit floating-point types.
+    use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64
+    implicit none
+
+    real(sp) :: float32
+    real(dp) :: float64
+
+    float32 = 1.0_sp ! Explicit suffix for literal constants
+    float64 = 1.0_dp
+
+    print *, 'Single precision float: ', float32
+    print *, 'Double repcision float: ', float64
+
+end program float
---
+++ read_value.f90
@@ -0,0 +1,9 @@
+program read_value
+    implicit none
+    integer :: age
+
+    print *, 'Please enter your age: '
+    read(*,*) age
+
+    print *, 'Your age is: ', age
+end program read_value
